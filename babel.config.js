63a5a578a51f0d7ab50c705508c44bde9a762c0b
tomibudis <tomibudis@gmail.com>
2021-04-28 14:16:54 +0700

    fix: prettier

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,13 @@
 module.exports = {
-  presets: [['@babel/preset-env', { loose: true }], '@babel/preset-react', '@babel/preset-typescript'],
+  presets: [
+    ['@babel/preset-env', { loose: true }],
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
   env: {
     test: {
       presets: [['@babel/preset-env', { loose: true, modules: 'commonjs' }]],
-      plugins: ['@babel/plugin-transform-runtime']
-    }
-  }
+      plugins: ['@babel/plugin-transform-runtime'],
+    },
+  },
 };
