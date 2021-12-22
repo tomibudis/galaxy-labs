9a24a33ee01973f3060a41c73ecc1084fb428b21
Tomi budi <tomibudis@gmail.com>
2021-12-22 23:04:16 +0700

    feat: select component (#10)

---
+++ src/components/Select/Select.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { cleanup, render } from '@testing-library/react';
+
+import Select from './Select';
+
+afterEach(() => {
+  cleanup();
+  mockOnClick.mockClear();
+});
+
+const mockOnClick = jest.fn();
+
+const SelectComponent = (props) => {
+  return <Select {...props} name="select-test" />;
+};
+
+describe('<Select />', () => {
+  it('render component', () => {
+    const { container } = render(<SelectComponent />);
+    expect(container.querySelector('input[name="select-test"]')).toBeTruthy();
+  });
+
+  it('matched with snapshot file', () => {
+    const { baseElement } = render(<SelectComponent />);
+    expect(baseElement).toMatchSnapshot();
+  });
+});
